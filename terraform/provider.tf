4a627061212634d463168c8d6b85f92f22257269
Mangulal <45239809+MangulalHome@users.noreply.github.com>
2025-08-01 09:11:28 -0500

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,7 @@
+
 locals {
   region = "us-east-1"
-  name   = "amonkincloud-cluster"
+  name   = "dnk-cloud-cluster"
   vpc_cidr = "10.123.0.0/16"
   azs      = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
@@ -10,7 +11,14 @@ locals {
     Example = local.name
   }
 }
-
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.30.0"
+    }
+  }
+}
 provider "aws" {
   region = "us-east-1"
 }
