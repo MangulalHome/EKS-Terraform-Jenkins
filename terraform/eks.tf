a77f44f2627bdd09e657fff5f2f147128a2f64cc
Mangulal <45239809+MangulalHome@users.noreply.github.com>
2025-08-01 09:27:24 -0500

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,9 +1,8 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
-}
 
-  cluster_name = local.name
+  cluster_name                    = local.name
   cluster_endpoint_public_access = true
 
   cluster_addons = {
@@ -22,7 +21,6 @@ module "eks" {
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
-  # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
     instance_types = ["m5.large"]
